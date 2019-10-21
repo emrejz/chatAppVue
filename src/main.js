fae43a11f71c98451f956f21e5ce5f982b3d68ee
emrejz <emre.kayacan@msn.com>
2019-10-21 21:14:29 +0300

    vuex ok

--- src/main.js
+++ src/main.js
@@ -2,16 +2,21 @@ import Vue from "vue";
 import App from "./App.vue";
 import dotenv from "dotenv";
 import VueRouter from "vue-router";
+import Vuex from "vuex";
 import routes from "./router";
+import storeOpt from "./stories/store";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 dotenv.config();
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
+Vue.use(Vuex);
 const router = new VueRouter(routes);
+const store = new Vuex.Store(storeOpt);
 
 new Vue({
   render: h => h(App),
-  router
+  router,
+  store
 }).$mount("#app");
